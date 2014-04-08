affb0c32f6a733ecdba4189b038e6f612b6d6eb6
mercyorangi <mercyorangi@gmail.com>
2014-04-08 19:02:32 +0300

    again!!!!

--- opop/src/upwards.java
+++ opop/src/upwards.java
@@ -7,7 +7,7 @@ public class upwards {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GIT is really really cool? going online?then?upupupup");
-
+		System.out.println("GIT ");
 	}
 
 }
