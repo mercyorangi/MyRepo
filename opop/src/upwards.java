97856a11d287fb45471aa058575f757ef3b2db01
mercyorangi <mercyorangi@gmail.com>
2014-04-08 18:56:57 +0300

    jijmkml

---
+++ opop/src/upwards.java
@@ -0,0 +1,13 @@
+
+public class upwards {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GIT is really really cool? going online?then?");
+
+	}
+
+}
